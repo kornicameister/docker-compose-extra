bee4f14b68de7694641894097dba88a56394f302
Tomasz Trebski <tomasz.trebski@ts.fujitsu.com>
2020-05-06 10:31:42 +0200

    fixup! Add remote feature install

--- install.sh
+++ install.sh
@@ -2,27 +2,27 @@
 
 ### adapted from wonderful: https://github.com/tj/git-extras/blob/master/install.sh
 
-command -v docker >/dev/null 2>&1 && (
-  dce::install() {
-    cd "${1}" || (
-      echo "Failed to enter ${1}"
-      exit 1
-    )
-    if [ -n "${PREFIX}" ]; then
-      PREFIX="${PREFIX}" make install
-    else
-      sudo make install
-    fi
-    cd ..
-  }
-  dce::clone() {
-    git clone "${CLONE_URL-"https://github.com/kornicameister/docker-compose-extra.git"}" \
-      --branch "${CLONE_BRANCH-master}" \
-      --depth "${CLONE_DEPTH-10}" \
-      "${1}" \
-      &>/dev/null
-  }
+dce::install() {
+  cd "${1}" || (
+    echo "Failed to enter ${1}"
+    exit 1
+  )
+  if [ -n "${PREFIX}" ]; then
+    PREFIX="${PREFIX}" make install
+  else
+    sudo make install
+  fi
+  cd ..
+}
+dce::clone() {
+  git clone "${CLONE_URL-"https://github.com/kornicameister/docker-compose-extra.git"}" \
+    --branch "${CLONE_BRANCH-master}" \
+    --depth "${CLONE_DEPTH-10}" \
+    "${1}" \
+    &>/dev/null
+}
 
+if command -v git >/dev/null 2>&1; then
   dir="$(mktemp -t -d dce-install.XXXXXXXXXX)"
   pwd="${PWD}"
 
@@ -34,7 +34,7 @@ command -v docker >/dev/null 2>&1 && (
 
   rm -rf "$dir"
   exit 0
-)
-
-echo "git is required to install like that"
-exit 1
+else
+  echo "git is required to install like that"
+  exit 1
+fi
