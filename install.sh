172d8910064ea8dfc90cdab1d29e667c2c6ee574
Tomasz Trebski <tomasz.trebski@ts.fujitsu.com>
2020-05-06 09:25:11 +0200

    fixup! Add remote feature install

--- install.sh
+++ install.sh
@@ -2,26 +2,33 @@
 
 ### adapted from wonderful: https://github.com/tj/git-extras/blob/master/install.sh
 
-dce::install() {
+command -v docker >/dev/null 2>&1 && (
+  dce::install() {
     if [ -n "${PREFIX}" ]; then
-        PREFIX="${PREFIX}" make -i "${1}" install
+      PREFIX="${PREFIX}" make -i "${1}" install
     else
-        sudo make -i "${1}" install
+      sudo make -i "${1}" install
     fi
-}
-dec::clone() {
-  git clone "${CLONE_URL-"https://github.com/kornicameister/docker-compose-extra.git"}"  \
-    --depth "${CLONE_DEPTH-10}" \
-    dce &> /dev/null
-}
+  }
+  dec::clone() {
+    git clone "${CLONE_URL-"https://github.com/kornicameister/docker-compose-extra.git"}" \
+      --depth "${CLONE_DEPTH-10}" \
+      "${1}" \
+      &>/dev/null
+  }
 
-dir="$(mktemp -t -d dce-install.XXXXXXXXXX)"
-pwd="${PWD}"
+  dir="$(mktemp -t -d dce-install.XXXXXXXXXX)"
+  pwd="${PWD}"
 
-cd "${dir}" && \
-  echo "Setting up 'git-extras'...." && \
-  dce:clone && \
-  make_install "${PWD}/dce" && \
-  cd "${pwd}" || exit 2
+  cd "${dir}" &&
+    echo "Setting up 'docker-compose-extra'" &&
+    dce:clone "${PWD}/dce" &&
+    dce::install "${PWD}/dce" &&
+    cd "${pwd}" || exit 2
 
-rm -rf "$dir"
+  rm -rf "$dir"
+  exit 0
+)
+
+echo "git is required to install like that"
+exit 1
